c9ca094fe1ac7b3b4b2a7d72996dd8be814ee1b2
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-01-15 20:11:53 -0300

    https://www.algaworks.com/aulas/1850/customizando-a-representacao-em-xml-com-wrapper-e-anotacoes-do-jackson/

--- src/main/java/br/com/siswbrasil/algafood/api/controller/CozinhaController.java
+++ src/main/java/br/com/siswbrasil/algafood/api/controller/CozinhaController.java
@@ -3,23 +3,36 @@ package br.com.siswbrasil.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.siswbrasil.algafood.api.model.CozinhasXmlWrapper;
 import br.com.siswbrasil.algafood.domain.model.Cozinha;
 import br.com.siswbrasil.algafood.domain.repository.CozinhaRepository;
 
 @RestController
 @RequestMapping("cozinhas")
 public class CozinhaController {
-	
+
 	@Autowired
 	private CozinhaRepository cozinhaRepository;
-	
+
 	@GetMapping
-	public List<Cozinha> listar(){
+	public List<Cozinha> listar() {
 		return cozinhaRepository.listar();
+	}
+	
+	@GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+	public CozinhasXmlWrapper listarXml() {
+		return new CozinhasXmlWrapper(cozinhaRepository.listar());
+	}	
+
+	@GetMapping("/{id}")
+	public Cozinha buscar(@PathVariable Long id) {
+		return cozinhaRepository.buscar(id);
 	}
 
 }
