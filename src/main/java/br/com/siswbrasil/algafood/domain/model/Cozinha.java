6a76444e304da02ca2adf7c8555c67ff89213639
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-02-13 23:44:25 -0300

    https://www.algaworks.com/aulas/1894/mapeando-relacionamento-bidirecional-com-onetomany/

--- src/main/java/br/com/siswbrasil/algafood/domain/model/Cozinha.java
+++ src/main/java/br/com/siswbrasil/algafood/domain/model/Cozinha.java
@@ -1,10 +1,16 @@
 package br.com.siswbrasil.algafood.domain.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -20,7 +26,11 @@ public class Cozinha {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@Column(nullable = false)
 	private String nome;
+
+	@JsonIgnore
+	@OneToMany(mappedBy = "cozinha")
+	private List<Restaurante> restaurantes = new ArrayList<>();
 }
