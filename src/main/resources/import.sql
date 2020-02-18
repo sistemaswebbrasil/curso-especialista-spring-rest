05ba87ffa4375f9bd720332f8135bfe0ab62a589
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-02-18 19:07:51 -0300

    https://www.algaworks.com/aulas/1895/mapeando-relacionamento-muitos-para-muitos-com-manytomany/

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,28 +1,25 @@
-#Cozinha 
 insert into cozinha (id, nome) values (1, 'Tailandesa');
 insert into cozinha (id, nome) values (2, 'Indiana');
 
-#Restaurante
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Gourmet', 10, 1);
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Thai Delivery', 9.50, 1);
-insert into restaurante (nome, taxa_frete, cozinha_id) values ('Tuk Tuk Comida Indiana', 15, 2);
+insert into restaurante (id, nome, taxa_frete, cozinha_id) values (1, 'Thai Gourmet', 10, 1);
+insert into restaurante (id, nome, taxa_frete, cozinha_id) values (2, 'Thai Delivery', 9.50, 1);
+insert into restaurante (id, nome, taxa_frete, cozinha_id) values (3, 'Tuk Tuk Comida Indiana', 15, 2);
 
-#Estado
-INSERT INTO estado (nome) VALUES ('Rio de Janeiro');
-INSERT INTO estado (nome) VALUES ('São Paulo');
-INSERT INTO estado (nome) VALUES ('Minas Gerais');
+insert into estado (id, nome) values (1, 'Minas Gerais');
+insert into estado (id, nome) values (2, 'São Paulo');
+insert into estado (id, nome) values (3, 'Ceará');
 
-#Cidade
-INSERT INTO cidade (nome, estado_id) VALUES ('Macaé', '1');
-INSERT INTO cidade (nome, estado_id) VALUES ('Santos', '2');
-INSERT INTO cidade (nome, estado_id) VALUES ('Juiz de Fora', '3');
+insert into cidade (id, nome, estado_id) values (1, 'Uberlândia', 1);
+insert into cidade (id, nome, estado_id) values (2, 'Belo Horizonte', 1);
+insert into cidade (id, nome, estado_id) values (3, 'São Paulo', 2);
+insert into cidade (id, nome, estado_id) values (4, 'Campinas', 2);
+insert into cidade (id, nome, estado_id) values (5, 'Fortaleza', 3);
 
-#Permissão
-INSERT INTO permissao (descricao, nome) VALUES ('Permite a criação , alteração , exclusão ', 'alteracao');
-INSERT INTO permissao (descricao, nome) VALUES ('Permite somente visualização', 'visualizacao');
+insert into forma_pagamento (id, descricao) values (1, 'Cartão de crédito');
+insert into forma_pagamento (id, descricao) values (2, 'Cartão de débito');
+insert into forma_pagamento (id, descricao) values (3, 'Dinheiro');
 
-#Forma Pagamento
-INSERT INTO forma_pagamento (descricao) VALUES ('Dinheiro');
-INSERT INTO forma_pagamento (descricao) VALUES ('Cartão de Crédito');
-INSERT INTO forma_pagamento (descricao) VALUES ('Cartão Débito');
-INSERT INTO forma_pagamento (descricao) VALUES ('Boleto Bancário');
+insert into permissao (id, nome, descricao) values (1, 'CONSULTAR_COZINHAS', 'Permite consultar cozinhas');
+insert into permissao (id, nome, descricao) values (2, 'EDITAR_COZINHAS', 'Permite editar cozinhas');
+
+insert into restaurante_forma_pagamento (restaurante_id, forma_pagamento_id) values (1, 1), (1, 2), (1, 3), (2, 3), (3, 2), (3, 3);
