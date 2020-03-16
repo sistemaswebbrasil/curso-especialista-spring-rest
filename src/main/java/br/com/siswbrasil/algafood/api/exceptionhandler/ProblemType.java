bce4f7382f20f94e1557adac733c830fe317d7f7
adriano <adriano@localhost>
2020-03-16 20:29:11 -0300

    https://www.algaworks.com/aulas/1951/desafio-tratando-exception-de-violacao-de-constraints-de-validacao/

--- src/main/java/br/com/siswbrasil/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/br/com/siswbrasil/algafood/api/exceptionhandler/ProblemType.java
@@ -4,13 +4,14 @@ import lombok.Getter;
 
 @Getter
 public enum ProblemType {
-
+	
+	DADOS_INVALIDOS("/dados-invalidos", "Dados inválidos"),
 	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
 	RECURSO_NAO_ENCONTRADO("/recurso-nao-encontrado", "Recurso não encontrado"),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
-	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"), 
-	PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"), 
-	ERRO_DE_SISTEMA("/erro-de-sistema","Erro interno do sistema");
+	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
+	PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido"),
+	ERRO_DE_SISTEMA("/erro-de-sistema", "Erro interno do sistema");
 
 	private String title;
 	private String uri;
