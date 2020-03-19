36cde0ad21d9b170a8135c409b9df1c39aa38f7c
adriano <adriano@localhost>
2020-03-19 11:04:00 -0300

    https://www.algaworks.com/aulas/1955/agrupando-e-restringindo-constraints-que-devem-ser-usadas-na-validacao/

---
+++ src/main/java/br/com/siswbrasil/algafood/Groups.java
@@ -0,0 +1,9 @@
+package br.com.siswbrasil.algafood;
+
+public interface Groups {
+
+	public interface CadastroRestaurante {
+
+	}
+
+}
--- src/main/java/br/com/siswbrasil/algafood/domain/model/Cozinha.java
+++ src/main/java/br/com/siswbrasil/algafood/domain/model/Cozinha.java
@@ -9,25 +9,27 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import br.com.siswbrasil.algafood.Groups;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
-import lombok.ToString;
 
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Cozinha {
 
-	@NotNull
+	@NotNull(groups = Groups.CadastroRestaurante.class)
 	@EqualsAndHashCode.Include
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotBlank
 	@Column(nullable = false)
 	private String nome;
 	
