538ec171098414a57683518653096613a55131ac
adria <adria@DESKTOP-FIU9TA2>
2020-04-07 10:29:01 -0300

    https://www.algaworks.com/aulas/1980/voltando-o-estado-inicial-do-banco-de-dados-para-cada-execucao-de-teste-com-callback-do-flyway/

--- src/test/java/br/com/siswbrasil/algafood/CadastroCozinhaIT.java
+++ src/test/java/br/com/siswbrasil/algafood/CadastroCozinhaIT.java
@@ -4,9 +4,11 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 
+import org.flywaydb.core.Flyway;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -22,11 +24,16 @@ public class CadastroCozinhaIT {
 	@LocalServerPort
 	private int port;
 	
+	@Autowired
+	private Flyway flyway;	
+	
 	@Before
 	public void setUp() {
 		RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
+		
+		flyway.migrate();
 	}	
 	
 	@Test
@@ -49,5 +56,17 @@ public class CadastroCozinhaIT {
 			.body("", hasSize(4))
 			.body("nome", hasItems("Indiana", "Tailandesa"));
 	}
+	
+	@Test
+	public void testRetornarStatus201_QuandoCadastrarCozinha() {
+		given()
+			.body("{ \"nome\": \"Chinesa\" }")
+			.contentType(ContentType.JSON)
+			.accept(ContentType.JSON)
+		.when()
+			.post()
+		.then()
+			.statusCode(HttpStatus.CREATED.value());
+	}	
 
 }
