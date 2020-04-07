6a9b93ac4de79166dd7e2f0cf0d42d72e520a515
adria <adria@DESKTOP-FIU9TA2>
2020-04-07 11:23:59 -0300

    https://www.algaworks.com/aulas/1982/limpando-e-populando-o-banco-de-dados-de-teste/

--- src/test/java/br/com/siswbrasil/algafood/CadastroCozinhaIT.java
+++ src/test/java/br/com/siswbrasil/algafood/CadastroCozinhaIT.java
@@ -1,10 +1,8 @@
 package br.com.siswbrasil.algafood;
 
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.hasItems;
 import static org.hamcrest.Matchers.hasSize;
 
-import org.flywaydb.core.Flyway;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -15,6 +13,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import br.com.siswbrasil.algafood.domain.model.Cozinha;
+import br.com.siswbrasil.algafood.domain.repository.CozinhaRepository;
+import br.com.siswbrasil.algafood.util.DatabaseCleaner;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
@@ -27,7 +28,10 @@ public class CadastroCozinhaIT {
 	private int port;
 	
 	@Autowired
-	private Flyway flyway;	
+	private DatabaseCleaner databaseCleaner;
+	
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
 	
 	@Before
 	public void setUp() {
@@ -35,8 +39,9 @@ public class CadastroCozinhaIT {
 		RestAssured.port = port;
 		RestAssured.basePath = "/cozinhas";
 		
-		flyway.migrate();
-	}	
+		databaseCleaner.clearTables();
+		prepararDados();
+	}
 	
 	@Test
 	public void deveRetornarStatus200_QuandoConsultarCozinhas() {
@@ -47,20 +52,19 @@ public class CadastroCozinhaIT {
 		.then()
 			.statusCode(HttpStatus.OK.value());
 	}
-	
+
 	@Test
-	public void deveConter4Cozinhas_QuandoConsultarCozinhas() {
+	public void deveConter2Cozinhas_QuandoConsultarCozinhas() {
 		given()
 			.accept(ContentType.JSON)
 		.when()
 			.get()
 		.then()
-			.body("", hasSize(4))
-			.body("nome", hasItems("Indiana", "Tailandesa"));
+			.body("", hasSize(2));
 	}
 	
 	@Test
-	public void testRetornarStatus201_QuandoCadastrarCozinha() {
+	public void deveRetornarStatus201_QuandoCadastrarCozinha() {
 		given()
 			.body("{ \"nome\": \"Chinesa\" }")
 			.contentType(ContentType.JSON)
@@ -69,6 +73,16 @@ public class CadastroCozinhaIT {
 			.post()
 		.then()
 			.statusCode(HttpStatus.CREATED.value());
-	}	
+	}
+	
+	private void prepararDados() {
+		Cozinha cozinha1 = new Cozinha();
+		cozinha1.setNome("Tailandesa");
+		cozinhaRepository.save(cozinha1);
 
+		Cozinha cozinha2 = new Cozinha();
+		cozinha2.setNome("Americana");
+		cozinhaRepository.save(cozinha2);
+	}
+	
 }
