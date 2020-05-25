9296b099087734c9b4a9e636bd5f60fb1987450a
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-05-25 09:43:58 -0300

    https://www.algaworks.com/aulas/2030/refatorando-o-codigo-de-regras-para-transicao-de-status-de-pedidos/

--- src/main/java/br/com/siswbrasil/algafood/domain/model/Pedido.java
+++ src/main/java/br/com/siswbrasil/algafood/domain/model/Pedido.java
@@ -20,6 +20,7 @@ import javax.persistence.OneToMany;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import br.com.siswbrasil.algafood.domain.exception.NegocioException;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
@@ -62,17 +63,43 @@ public class Pedido {
 	@JoinColumn(name = "usuario_cliente_id", nullable = false)
 	private Usuario cliente;
 	
-	@OneToMany(mappedBy = "pedido",cascade = CascadeType.ALL)
+	@OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL)
 	private List<ItemPedido> itens = new ArrayList<>();
-	
-	public void calcularValorTotal() {
-	    getItens().forEach(ItemPedido::calcularPrecoTotal);
-	    
-	    this.subtotal = getItens().stream()
-	        .map(item -> item.getPrecoTotal())
-	        .reduce(BigDecimal.ZERO, BigDecimal::add);
-	    
-	    this.valorTotal = this.subtotal.add(this.taxaFrete);
-	}	
 
+	public void calcularValorTotal() {
+		getItens().forEach(ItemPedido::calcularPrecoTotal);
+		
+		this.subtotal = getItens().stream()
+			.map(item -> item.getPrecoTotal())
+			.reduce(BigDecimal.ZERO, BigDecimal::add);
+		
+		this.valorTotal = this.subtotal.add(this.taxaFrete);
+	}
+	
+	public void confirmar() {
+		setStatus(StatusPedido.CONFIRMADO);
+		setDataConfirmacao(OffsetDateTime.now());
+	}
+	
+	public void entregar() {
+		setStatus(StatusPedido.ENTREGUE);
+		setDataEntrega(OffsetDateTime.now());
+	}
+	
+	public void cancelar() {
+		setStatus(StatusPedido.CANCELADO);
+		setDataCancelamento(OffsetDateTime.now());
+	}
+	
+	private void setStatus(StatusPedido novoStatus) {
+		if (getStatus().naoPodeAlterarPara(novoStatus)) {
+			throw new NegocioException(
+					String.format("Status do pedido %d não pode ser alterado de %s para %s",
+							getId(), getStatus().getDescricao(), 
+							novoStatus.getDescricao()));
+		}
+		
+		this.status = novoStatus;
+	}
+	
 }
