1fd27389e39beeebf76e71e676517321d515f055
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-05-28 20:28:32 -0300

    https://www.algaworks.com/aulas/2035/limitando-os-campos-retornados-pela-api-com-squiggly/

--- src/main/java/br/com/siswbrasil/algafood/api/controller/PedidoController.java
+++ src/main/java/br/com/siswbrasil/algafood/api/controller/PedidoController.java
@@ -4,15 +4,22 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+
 
 import br.com.siswbrasil.algafood.api.assembler.PedidoInputDisassembler;
 import br.com.siswbrasil.algafood.api.assembler.PedidoModelAssembler;
@@ -45,7 +52,7 @@ public class PedidoController {
 	
 	@Autowired
 	private PedidoInputDisassembler pedidoInputDisassembler;	
-    
+	
 	@GetMapping
 	public List<PedidoResumoModel> listar() {
 		List<Pedido> todosPedidos = pedidoRepository.findAll();
--- src/main/java/br/com/siswbrasil/algafood/api/model/PedidoResumoModel.java
+++ src/main/java/br/com/siswbrasil/algafood/api/model/PedidoResumoModel.java
@@ -3,6 +3,8 @@ package br.com.siswbrasil.algafood.api.model;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
+import com.fasterxml.jackson.annotation.JsonFilter;
+
 import lombok.Getter;
 import lombok.Setter;
 
