6e8c0ee54c6ffb4522c17b3366e54f02cd5be803
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-06-04 09:55:40 -0300

    https://www.algaworks.com/aulas/2041/desafio-implementando-paginacao-e-ordenacao-de-pedidos/

--- src/main/java/br/com/siswbrasil/algafood/api/controller/PedidoController.java
+++ src/main/java/br/com/siswbrasil/algafood/api/controller/PedidoController.java
@@ -5,6 +5,10 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,50 +37,55 @@ import br.com.siswbrasil.algafood.infrastructure.repository.spec.PedidoSpecs;
 @RequestMapping(value = "/pedidos")
 public class PedidoController {
 
-    @Autowired
-    private PedidoRepository pedidoRepository;
-    
-    @Autowired
-    private EmissaoPedidoService emissaoPedido;
-    
-    @Autowired
-    private PedidoModelAssembler pedidoModelAssembler;
-    
 	@Autowired
-	private PedidoResumoModelAssembler pedidoResumoModelAssembler;  
-	
+	private PedidoRepository pedidoRepository;
+
 	@Autowired
-	private PedidoInputDisassembler pedidoInputDisassembler;	
-	
-	@GetMapping  
-	public List<PedidoResumoModel> pesquisar(PedidoFilter filtro) {
-		List<Pedido> todosPedidos = pedidoRepository.findAll(PedidoSpecs.usandoFiltro(filtro));
-		
-		return pedidoResumoModelAssembler.toCollectionModel(todosPedidos);
+	private EmissaoPedidoService emissaoPedido;
+
+	@Autowired
+	private PedidoModelAssembler pedidoModelAssembler;
+
+	@Autowired
+	private PedidoResumoModelAssembler pedidoResumoModelAssembler;
+
+	@Autowired
+	private PedidoInputDisassembler pedidoInputDisassembler;
+
+	@GetMapping
+	public Page<PedidoResumoModel> pesquisar(PedidoFilter filtro, @PageableDefault(size = 10) Pageable pageable) {
+		Page<Pedido> pedidosPage = pedidoRepository.findAll(PedidoSpecs.usandoFiltro(filtro), pageable);
+
+		List<PedidoResumoModel> pedidosModel = pedidoResumoModelAssembler.toCollectionModel(pedidosPage.getContent());
+
+		Page<PedidoResumoModel> pedidosModelPage = new PageImpl<>(pedidosModel, pageable,
+				pedidosPage.getTotalElements());
+
+		return pedidosModelPage;
 	}
-	
+
 	@GetMapping("/{codigoPedido}")
 	public PedidoModel buscar(@PathVariable String codigoPedido) {
 		Pedido pedido = emissaoPedido.buscarOuFalhar(codigoPedido);
-		
+
 		return pedidoModelAssembler.toModel(pedido);
-	}  
-	
+	}
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public PedidoModel adicionar(@Valid @RequestBody PedidoInput pedidoInput) {
-	    try {
-	        Pedido novoPedido = pedidoInputDisassembler.toDomainObject(pedidoInput);
+		try {
+			Pedido novoPedido = pedidoInputDisassembler.toDomainObject(pedidoInput);
 
-	        // TODO pegar usuário autenticado
-	        novoPedido.setCliente(new Usuario());
-	        novoPedido.getCliente().setId(1L);
+			// TODO pegar usuário autenticado
+			novoPedido.setCliente(new Usuario());
+			novoPedido.getCliente().setId(1L);
 
-	        novoPedido = emissaoPedido.emitir(novoPedido);
+			novoPedido = emissaoPedido.emitir(novoPedido);
 
-	        return pedidoModelAssembler.toModel(novoPedido);
-	    } catch (EntidadeNaoEncontradaException e) {
-	        throw new NegocioException(e.getMessage(), e);
-	    }
-	}	
-}           
+			return pedidoModelAssembler.toModel(novoPedido);
+		} catch (EntidadeNaoEncontradaException e) {
+			throw new NegocioException(e.getMessage(), e);
+		}
+	}
+}
