2c3773eaf25735aeddadc6d66d30a7330823b3b7
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-06-12 19:26:21 -0300

    https://www.algaworks.com/aulas/2134/descrevendo-media-type-da-resposta-nos-endpoints/

--- src/main/java/br/com/siswbrasil/algafood/api/controller/CidadeController.java
+++ src/main/java/br/com/siswbrasil/algafood/api/controller/CidadeController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,7 +31,7 @@ import io.swagger.annotations.Api;
 
 @Api(tags = "Cidades")
 @RestController
-@RequestMapping(value = "/cidades")
+@RequestMapping(path = "/cidades", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
 public class CidadeController implements CidadeControllerOpenApi {
 
 	@Autowired
--- src/main/java/br/com/siswbrasil/algafood/api/controller/GrupoController.java
+++ src/main/java/br/com/siswbrasil/algafood/api/controller/GrupoController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,7 +27,7 @@ import br.com.siswbrasil.algafood.domain.repository.GrupoRepository;
 import br.com.siswbrasil.algafood.domain.service.GrupoService;
 
 @RestController
-@RequestMapping("/grupos")
+@RequestMapping(path = "/grupos" , produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
 public class GrupoController implements GrupoControllerOpenApi{
 
 	@Autowired
