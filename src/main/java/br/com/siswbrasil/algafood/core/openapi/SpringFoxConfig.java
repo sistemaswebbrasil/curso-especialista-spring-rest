31550861d62a86dc4710f05d225c75ee8b49d454
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-06-12 18:54:54 -0300

    https://www.algaworks.com/aulas/2129/descrevendo-o-modelo-de-representacao-de-problema/

--- src/main/java/br/com/siswbrasil/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/br/com/siswbrasil/algafood/core/openapi/SpringFoxConfig.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.fasterxml.classmate.TypeResolver;
+
+import br.com.siswbrasil.algafood.api.exceptionhandler.Problem;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -33,6 +36,8 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 
 	@Bean
 	public Docket apiDocket() {
+		var typeResolver = new TypeResolver();
+		
 		return new Docket(DocumentationType.SWAGGER_2)
 				.select()
 					.apis(RequestHandlerSelectors.basePackage("br.com.siswbrasil.algafood.api"))
@@ -42,7 +47,8 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 				.globalResponseMessage(RequestMethod.GET, globalGetResponseMessages())
 		        .globalResponseMessage(RequestMethod.POST, globalPostPutResponseMessages())
 		        .globalResponseMessage(RequestMethod.PUT, globalPostPutResponseMessages())
-		        .globalResponseMessage(RequestMethod.DELETE, globalDeleteResponseMessages())				
+		        .globalResponseMessage(RequestMethod.DELETE, globalDeleteResponseMessages())
+		        .additionalModels(typeResolver.resolve(Problem.class))
 				.apiInfo(apiInfo())
 				.tags(new Tag("Cidades", "Gerencia as cidades"));				
 	}
