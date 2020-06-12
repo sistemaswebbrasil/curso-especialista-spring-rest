e31cd00c782a4937ece83ea06ea79adeed322ead
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-06-12 19:01:50 -0300

    https://www.algaworks.com/aulas/2130/referenciando-modelo-de-representacao-de-problema-com-codigos-de-status-de-erro/

--- src/main/java/br/com/siswbrasil/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/br/com/siswbrasil/algafood/core/openapi/SpringFoxConfig.java
@@ -19,6 +19,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.ResponseMessage;
@@ -58,6 +59,7 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 				new ResponseMessageBuilder()
 					.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
 					.message("Erro interno do servidor")
+					.responseModel(new ModelRef("Problema"))
 					.build(),
 				new ResponseMessageBuilder()
 					.code(HttpStatus.NOT_ACCEPTABLE.value())
@@ -67,38 +69,43 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 	}
 	
 	private List<ResponseMessage> globalPostPutResponseMessages() {
-	    return Arrays.asList(
-	            new ResponseMessageBuilder()
-	                .code(HttpStatus.BAD_REQUEST.value())
-	                .message("Requisição inválida (erro do cliente)")
-	                .build(),
-	            new ResponseMessageBuilder()
-	                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
-	                .message("Erro interno no servidor")
-	                .build(),
-	            new ResponseMessageBuilder()
-	                .code(HttpStatus.NOT_ACCEPTABLE.value())
-	                .message("Recurso não possui representação que poderia ser aceita pelo consumidor")
-	                .build(),
-	            new ResponseMessageBuilder()
-	                .code(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value())
-	                .message("Requisição recusada porque o corpo está em um formato não suportado")
-	                .build()
-	        );
+		return Arrays.asList(
+				new ResponseMessageBuilder()
+					.code(HttpStatus.BAD_REQUEST.value())
+					.message("Requisição inválida (erro do cliente)")
+					.responseModel(new ModelRef("Problema"))
+					.build(),
+				new ResponseMessageBuilder()
+					.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+					.message("Erro interno no servidor")
+					.responseModel(new ModelRef("Problema"))
+					.build(),
+				new ResponseMessageBuilder()
+					.code(HttpStatus.NOT_ACCEPTABLE.value())
+					.message("Recurso não possui representação que poderia ser aceita pelo consumidor")
+					.build(),
+				new ResponseMessageBuilder()
+					.code(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value())
+					.message("Requisição recusada porque o corpo está em um formato não suportado")
+					.responseModel(new ModelRef("Problema"))
+					.build()
+			);
 	}
-
+	
 	private List<ResponseMessage> globalDeleteResponseMessages() {
-	    return Arrays.asList(
-	            new ResponseMessageBuilder()
-	                .code(HttpStatus.BAD_REQUEST.value())
-	                .message("Requisição inválida (erro do cliente)")
-	                .build(),
-	            new ResponseMessageBuilder()
-	                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
-	                .message("Erro interno no servidor")
-	                .build()
-	        );
-	}	
+		return Arrays.asList(
+				new ResponseMessageBuilder()
+					.code(HttpStatus.BAD_REQUEST.value())
+					.message("Requisição inválida (erro do cliente)")
+					.responseModel(new ModelRef("Problema"))
+					.build(),
+				new ResponseMessageBuilder()
+					.code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+					.message("Erro interno no servidor")
+					.responseModel(new ModelRef("Problema"))
+					.build()
+			);
+	}
 	
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
