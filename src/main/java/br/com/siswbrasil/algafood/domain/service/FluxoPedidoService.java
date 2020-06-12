4be3e4dcad73f7af6da1acd95e05a9e9d9458c6f
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-06-12 11:08:50 -0300

    https://www.algaworks.com/aulas/2083/usando-o-servico-de-envio-de-e-mails-na-confirmacao-de-pedidos/

--- src/main/java/br/com/siswbrasil/algafood/domain/service/FluxoPedidoService.java
+++ src/main/java/br/com/siswbrasil/algafood/domain/service/FluxoPedidoService.java
@@ -6,29 +6,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.siswbrasil.algafood.domain.model.Pedido;
+import br.com.siswbrasil.algafood.domain.service.EnvioEmailService.Mensagem;
 
 @Service
 public class FluxoPedidoService {
 
 	@Autowired
 	private EmissaoPedidoService emissaoPedido;
-	
+
+	@Autowired
+	private EnvioEmailService envioEmail;
+
 	@Transactional
 	public void confirmar(String codigoPedido) {
 		Pedido pedido = emissaoPedido.buscarOuFalhar(codigoPedido);
 		pedido.confirmar();
+
+		var mensagem = Mensagem.builder()
+				.assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
+				.corpo("O pedidod de código <strong>" + pedido.getCodigo() + "</strong> foi confirmado!")
+				.destinatario(pedido.getCliente().getEmail())
+				.build();
+
+		envioEmail.enviar(mensagem);
 	}
-	
+
 	@Transactional
 	public void cancelar(String codigoPedido) {
 		Pedido pedido = emissaoPedido.buscarOuFalhar(codigoPedido);
 		pedido.cancelar();
 	}
-	
+
 	@Transactional
 	public void entregar(String codigoPedido) {
 		Pedido pedido = emissaoPedido.buscarOuFalhar(codigoPedido);
 		pedido.entregar();
 	}
-	
+
 }
