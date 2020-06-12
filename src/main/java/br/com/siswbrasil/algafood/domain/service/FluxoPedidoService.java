4c03e89d8b5ed2e81d449972ad7784237177b3da
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-06-12 12:56:52 -0300

    https://www.algaworks.com/aulas/2092/reagindo-a-domain-events-em-fases-especificas-da-transacao/

--- src/main/java/br/com/siswbrasil/algafood/domain/service/FluxoPedidoService.java
+++ src/main/java/br/com/siswbrasil/algafood/domain/service/FluxoPedidoService.java
@@ -6,30 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.siswbrasil.algafood.domain.model.Pedido;
-import br.com.siswbrasil.algafood.domain.service.EnvioEmailService.Mensagem;
+import br.com.siswbrasil.algafood.domain.repository.PedidoRepository;
 
 @Service
 public class FluxoPedidoService {
 
 	@Autowired
 	private EmissaoPedidoService emissaoPedido;
-
+	
 	@Autowired
-	private EnvioEmailService envioEmail;
+	private PedidoRepository pedidoRepository;
 
 	@Transactional
 	public void confirmar(String codigoPedido) {
 		Pedido pedido = emissaoPedido.buscarOuFalhar(codigoPedido);
 		pedido.confirmar();
-
-		var mensagem = Mensagem.builder()
-				.assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
-				.corpo("pedido-confirmado.html")
-				.variavel("pedido", pedido)				
-				.destinatario(pedido.getCliente().getEmail())
-				.build();
-
-		envioEmail.enviar(mensagem);
+		
+		pedidoRepository.save(pedido);
 	}
 
 	@Transactional
