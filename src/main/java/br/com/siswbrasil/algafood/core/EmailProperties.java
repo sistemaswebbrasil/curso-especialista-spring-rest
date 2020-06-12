c298a56a218d56d146a12c4debea82a742ea1e61
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-06-12 10:43:02 -0300

    https://www.algaworks.com/aulas/2082/implementando-o-servico-de-infraestrutura-de-envio-de-e-mails-com-spring/

---
+++ src/main/java/br/com/siswbrasil/algafood/core/EmailProperties.java
@@ -0,0 +1,23 @@
+package br.com.siswbrasil.algafood.core;
+
+import javax.validation.constraints.NotNull;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Validated
+@Getter
+@Setter
+@Component
+@ConfigurationProperties("algafood.email")
+public class EmailProperties {
+	
+	@NotNull
+	private String remetente;
+	
+
+}
