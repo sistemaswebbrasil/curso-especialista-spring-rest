b26010aaa589362e7c29d5f0bf9e36fca83892f1
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-06-12 18:42:24 -0300

    https://www.algaworks.com/aulas/2128/desafio-descrevendo-codigos-de-status-de-respostas-de-forma-global/

--- src/main/java/br/com/siswbrasil/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/br/com/siswbrasil/algafood/core/openapi/SpringFoxConfig.java
@@ -40,6 +40,9 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 					.build()
 				.useDefaultResponseMessages(false)
 				.globalResponseMessage(RequestMethod.GET, globalGetResponseMessages())
+		        .globalResponseMessage(RequestMethod.POST, globalPostPutResponseMessages())
+		        .globalResponseMessage(RequestMethod.PUT, globalPostPutResponseMessages())
+		        .globalResponseMessage(RequestMethod.DELETE, globalDeleteResponseMessages())				
 				.apiInfo(apiInfo())
 				.tags(new Tag("Cidades", "Gerencia as cidades"));				
 	}
@@ -56,6 +59,40 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 					.build()
 			);
 	}
+	
+	private List<ResponseMessage> globalPostPutResponseMessages() {
+	    return Arrays.asList(
+	            new ResponseMessageBuilder()
+	                .code(HttpStatus.BAD_REQUEST.value())
+	                .message("Requisição inválida (erro do cliente)")
+	                .build(),
+	            new ResponseMessageBuilder()
+	                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+	                .message("Erro interno no servidor")
+	                .build(),
+	            new ResponseMessageBuilder()
+	                .code(HttpStatus.NOT_ACCEPTABLE.value())
+	                .message("Recurso não possui representação que poderia ser aceita pelo consumidor")
+	                .build(),
+	            new ResponseMessageBuilder()
+	                .code(HttpStatus.UNSUPPORTED_MEDIA_TYPE.value())
+	                .message("Requisição recusada porque o corpo está em um formato não suportado")
+	                .build()
+	        );
+	}
+
+	private List<ResponseMessage> globalDeleteResponseMessages() {
+	    return Arrays.asList(
+	            new ResponseMessageBuilder()
+	                .code(HttpStatus.BAD_REQUEST.value())
+	                .message("Requisição inválida (erro do cliente)")
+	                .build(),
+	            new ResponseMessageBuilder()
+	                .code(HttpStatus.INTERNAL_SERVER_ERROR.value())
+	                .message("Erro interno no servidor")
+	                .build()
+	        );
+	}	
 	
 	private ApiInfo apiInfo() {
 		return new ApiInfoBuilder()
