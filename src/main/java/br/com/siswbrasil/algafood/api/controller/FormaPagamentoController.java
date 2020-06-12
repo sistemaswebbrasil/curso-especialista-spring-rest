ff45d062d17e16a3018cba1fd8599bf13da99b5c
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-06-12 16:37:19 -0300

    https://www.algaworks.com/aulas/2111/adicionando-outras-diretivas-de-cache-control-na-resposta-http/

--- src/main/java/br/com/siswbrasil/algafood/api/controller/FormaPagamentoController.java
+++ src/main/java/br/com/siswbrasil/algafood/api/controller/FormaPagamentoController.java
@@ -1,11 +1,14 @@
 package br.com.siswbrasil.algafood.api.controller;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -41,17 +44,30 @@ public class FormaPagamentoController {
 	private FormaPagamentoInputDisassembler formaPagamentoInputDisassembler;
 
 	@GetMapping
-	public List<FormaPagamentoModel> listar() {
-		List<FormaPagamento> todasFormaPagamento = formaPagamentoRepository.findAll();
-
-		return formaPagamentoModelAssembler.toCollectionModel(todasFormaPagamento);
+	public ResponseEntity<List<FormaPagamentoModel>> listar() {
+		List<FormaPagamento> todasFormasPagamentos = formaPagamentoRepository.findAll();
+		
+		List<FormaPagamentoModel> formasPagamentosModel = formaPagamentoModelAssembler
+				.toCollectionModel(todasFormasPagamentos);
+		
+		return ResponseEntity.ok()
+				.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+//				.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePrivate())
+//				.cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS).cachePublic())
+//				.cacheControl(CacheControl.noCache())
+//				.cacheControl(CacheControl.noStore())
+				.body(formasPagamentosModel);
 	}
 
 	@GetMapping("/{formaPagamentoId}")
-	public FormaPagamentoModel buscar(@PathVariable Long formaPagamentoId) {
-		FormaPagamento formaPagamento = formaPagamentoService.buscarOuFalhar(formaPagamentoId);
-
-		return formaPagamentoModelAssembler.toModel(formaPagamento);
+	public ResponseEntity<FormaPagamentoModel> buscar(@PathVariable Long formaPagamentoId) {
+	  FormaPagamento formaPagamento = formaPagamentoService.buscarOuFalhar(formaPagamentoId);
+	  
+	  FormaPagamentoModel formaPagamentoModel =  formaPagamentoModelAssembler.toModel(formaPagamento);
+	  
+	  return ResponseEntity.ok()
+	      .cacheControl(CacheControl.maxAge(10, TimeUnit.SECONDS))
+	      .body(formaPagamentoModel);
 	}
 
 	@PostMapping
--- src/main/java/br/com/siswbrasil/algafood/core/web/WebConfig.java
+++ src/main/java/br/com/siswbrasil/algafood/core/web/WebConfig.java
@@ -1,6 +1,10 @@
 package br.com.siswbrasil.algafood.core.web;
 
+import javax.servlet.Filter;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -9,10 +13,14 @@ public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**")
-			.allowedMethods("*");
+		registry.addMapping("/**").allowedMethods("*");
 //			.allowedOrigins("*")
 //			.maxAge(30);
 	}
-	
+
+	@Bean  
+	public Filter shallowEtagHeaderFilter() {
+		return new ShallowEtagHeaderFilter();
+	}
+
 }
