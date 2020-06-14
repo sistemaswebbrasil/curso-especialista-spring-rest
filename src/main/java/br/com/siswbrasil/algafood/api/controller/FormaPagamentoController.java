8457a51277ed1118666a3a52f7a4eb3bb115394b
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-06-14 12:04:51 -0300

    https://www.algaworks.com/aulas/2139/desafio-descrevendo-documentacao-de-endpoints-de-formas-de-pagamento/

--- src/main/java/br/com/siswbrasil/algafood/api/controller/FormaPagamentoController.java
+++ src/main/java/br/com/siswbrasil/algafood/api/controller/FormaPagamentoController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,13 +27,14 @@ import br.com.siswbrasil.algafood.api.assembler.FormaPagamentoInputDisassembler;
 import br.com.siswbrasil.algafood.api.assembler.FormaPagamentoModelAssembler;
 import br.com.siswbrasil.algafood.api.model.FormaPagamentoModel;
 import br.com.siswbrasil.algafood.api.model.input.FormaPagamentoInput;
+import br.com.siswbrasil.algafood.api.openapi.controller.FormaPagamentoControllerOpenApi;
 import br.com.siswbrasil.algafood.domain.model.FormaPagamento;
 import br.com.siswbrasil.algafood.domain.repository.FormaPagamentoRepository;
 import br.com.siswbrasil.algafood.domain.service.FormaPagamentoService;
 
 @RestController
-@RequestMapping("/formas-pagamento")
-public class FormaPagamentoController {
+@RequestMapping(path = "/formas-pagamento", produces = MediaType.APPLICATION_JSON_VALUE)
+public class FormaPagamentoController implements FormaPagamentoControllerOpenApi {
 
 	@Autowired
 	private FormaPagamentoRepository formaPagamentoRepository;
