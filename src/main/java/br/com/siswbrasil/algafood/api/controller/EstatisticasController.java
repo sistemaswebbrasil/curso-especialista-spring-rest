748ae850645ea4e332af122f026e42e2465613ec
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-06-14 15:45:55 -0300

    https://www.algaworks.com/aulas/2155/desafio-descrevendo-documentacao-de-endpoint-de-estatisticas/

--- src/main/java/br/com/siswbrasil/algafood/api/controller/EstatisticasController.java
+++ src/main/java/br/com/siswbrasil/algafood/api/controller/EstatisticasController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.siswbrasil.algafood.api.openapi.controller.EstatisticasControllerOpenApi;
 import br.com.siswbrasil.algafood.domain.filter.VendaDiariaFilter;
 import br.com.siswbrasil.algafood.domain.model.dto.VendaDiaria;
 import br.com.siswbrasil.algafood.domain.service.VendaQueryService;
@@ -18,7 +19,7 @@ import br.com.siswbrasil.algafood.domain.service.VendaReportService;
 
 @RestController
 @RequestMapping(path = "/estatisticas")
-public class EstatisticasController {
+public class EstatisticasController implements EstatisticasControllerOpenApi {
 
 	@Autowired
 	private VendaQueryService vendaQueryService;
