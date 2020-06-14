8da2a17932731005a6d2a62bd52307c9fba6543f
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-06-14 14:54:00 -0300

    https://www.algaworks.com/aulas/2149/desafio-descrevendo-documentacao-de-endpoints-de-produtos/

--- src/main/java/br/com/siswbrasil/algafood/api/controller/RestauranteProdutoController.java
+++ src/main/java/br/com/siswbrasil/algafood/api/controller/RestauranteProdutoController.java
@@ -6,12 +6,14 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +21,7 @@ import br.com.siswbrasil.algafood.api.assembler.ProdutoInputDisassembler;
 import br.com.siswbrasil.algafood.api.assembler.ProdutoModelAssembler;
 import br.com.siswbrasil.algafood.api.model.ProdutoModel;
 import br.com.siswbrasil.algafood.api.model.input.ProdutoInput;
+import br.com.siswbrasil.algafood.api.openapi.controller.RestauranteProdutoControllerOpenApi;
 import br.com.siswbrasil.algafood.domain.model.Produto;
 import br.com.siswbrasil.algafood.domain.model.Restaurante;
 import br.com.siswbrasil.algafood.domain.repository.ProdutoRepository;
@@ -26,8 +29,9 @@ import br.com.siswbrasil.algafood.domain.service.ProdutoService;
 import br.com.siswbrasil.algafood.domain.service.RestauranteService;
 
 @RestController
-@RequestMapping("/restaurantes/{restauranteId}/produtos")
-public class RestauranteProdutoController {
+@RequestMapping(path = "/restaurantes/{restauranteId}/produtos", 
+    produces = MediaType.APPLICATION_JSON_VALUE)
+public class RestauranteProdutoController implements RestauranteProdutoControllerOpenApi { 
 
 	@Autowired
 	private ProdutoRepository produtoRepository;
@@ -44,44 +48,57 @@ public class RestauranteProdutoController {
 	@Autowired
 	private ProdutoInputDisassembler produtoInputDisassembler;
 
+	@Override
 	@GetMapping
-	public List<ProdutoModel> listar(@PathVariable Long restauranteId) {
+	public List<ProdutoModel> listar(@PathVariable Long restauranteId,
+			@RequestParam(required = false) boolean incluirInativos) {
 		Restaurante restaurante = restauranteService.buscarOuFalhar(restauranteId);
-
-		List<Produto> todosProdutos = produtoRepository.findTodosByRestaurante(restaurante);
-
+		
+		List<Produto> todosProdutos = null;
+		
+		if (incluirInativos) {
+			todosProdutos = produtoRepository.findTodosByRestaurante(restaurante);
+		} else {
+			todosProdutos = produtoRepository.findAtivosByRestaurante(restaurante);
+		}
+		
 		return produtoModelAssembler.toCollectionModel(todosProdutos);
 	}
-
+	
+	@Override
 	@GetMapping("/{produtoId}")
 	public ProdutoModel buscar(@PathVariable Long restauranteId, @PathVariable Long produtoId) {
 		Produto produto = produtoService.buscarOuFalhar(restauranteId, produtoId);
-
+		
 		return produtoModelAssembler.toModel(produto);
 	}
-
+	
+	@Override
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public ProdutoModel adicionar(@PathVariable Long restauranteId, @RequestBody @Valid ProdutoInput produtoInput) {
+	public ProdutoModel adicionar(@PathVariable Long restauranteId,
+			@RequestBody @Valid ProdutoInput produtoInput) {
 		Restaurante restaurante = restauranteService.buscarOuFalhar(restauranteId);
-
+		
 		Produto produto = produtoInputDisassembler.toDomainObject(produtoInput);
 		produto.setRestaurante(restaurante);
-
+		
 		produto = produtoService.salvar(produto);
-
+		
 		return produtoModelAssembler.toModel(produto);
 	}
-
+	
+	@Override
 	@PutMapping("/{produtoId}")
 	public ProdutoModel atualizar(@PathVariable Long restauranteId, @PathVariable Long produtoId,
 			@RequestBody @Valid ProdutoInput produtoInput) {
 		Produto produtoAtual = produtoService.buscarOuFalhar(restauranteId, produtoId);
-
+		
 		produtoInputDisassembler.copyToDomainObject(produtoInput, produtoAtual);
-
+		
 		produtoAtual = produtoService.salvar(produtoAtual);
-
+		
 		return produtoModelAssembler.toModel(produtoAtual);
 	}
+	
 }
--- src/main/java/br/com/siswbrasil/algafood/api/model/input/ProdutoInput.java
+++ src/main/java/br/com/siswbrasil/algafood/api/model/input/ProdutoInput.java
@@ -6,24 +6,30 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PositiveOrZero;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
+
 
 @Setter
 @Getter
 public class ProdutoInput {
 
-    @NotBlank
-    private String nome;
-    
-    @NotBlank
-    private String descricao;
-    
-    @NotNull
-    @PositiveOrZero
-    private BigDecimal preco;
-    
-    @NotNull
-    private Boolean ativo;
-    
+	@ApiModelProperty(example = "Espetinho de Cupim", required = true)
+	@NotBlank
+	private String nome;
+	
+	@ApiModelProperty(example = "Acompanha farinha, mandioca e vinagrete", required = true)
+	@NotBlank
+	private String descricao;
+	
+	@ApiModelProperty(example = "12.50", required = true)
+	@NotNull
+	@PositiveOrZero
+	private BigDecimal preco;
+	
+	@ApiModelProperty(example = "true", required = true)
+	@NotNull
+	private Boolean ativo;
+	
 }
