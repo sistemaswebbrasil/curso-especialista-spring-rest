a7b75c036923413dacc5fbff4adfe45de787612b
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-06-14 15:33:18 -0300

    https://www.algaworks.com/aulas/2151/corrigindo-documentacao-no-swagger-ui-para-upload-de-arquivos/

--- src/main/java/br/com/siswbrasil/algafood/api/openapi/controller/RestauranteProdutoFotoControllerOpenApi.java
+++ src/main/java/br/com/siswbrasil/algafood/api/openapi/controller/RestauranteProdutoFotoControllerOpenApi.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
+import org.springframework.web.multipart.MultipartFile;
 
 import br.com.siswbrasil.algafood.api.exceptionhandler.Problem;
 import br.com.siswbrasil.algafood.api.model.FotoProdutoModel;
@@ -17,26 +18,24 @@ import io.swagger.annotations.ApiResponses;
 @Api(tags = "Produtos")
 public interface RestauranteProdutoFotoControllerOpenApi {
 
-    @ApiOperation("Atualiza a foto do produto de um restaurante")
-    @ApiResponses({
-        @ApiResponse(code = 200, message = "Foto do produto atualizada"),
-        @ApiResponse(code = 404, message = "Produto de restaurante não encontrado", response = Problem.class)
-    })
-    FotoProdutoModel atualizarFoto(
-            @ApiParam(value = "ID do restaurante", example = "1", required = true)
-            Long restauranteId,
-            
-            @ApiParam(value = "ID do produto", example = "1", required = true)
-            Long produtoId,
-            
-            FotoProdutoInput fotoProdutoInput) throws IOException;
-
-    @ApiOperation("Exclui a foto do produto de um restaurante")
-    @ApiResponses({
-        @ApiResponse(code = 204, message = "Foto do produto excluída"),
-        @ApiResponse(code = 400, message = "ID do restaurante ou produto inválido", response = Problem.class),
-        @ApiResponse(code = 404, message = "Foto de produto não encontrada", response = Problem.class)
-    })
+	@ApiOperation("Atualiza a foto do produto de um restaurante")
+	@ApiResponses({
+		@ApiResponse(code = 200, message = "Foto do produto atualizada"),
+		@ApiResponse(code = 404, message = "Produto de restaurante não encontrado", response = Problem.class)
+	})
+	FotoProdutoModel atualizarFoto(
+			@ApiParam(value = "ID do restaurante", example = "1", required = true)
+			Long restauranteId,
+			
+			@ApiParam(value = "ID do produto", example = "1", required = true)
+			Long produtoId,
+			
+			FotoProdutoInput fotoProdutoInput,
+			
+			@ApiParam(value = "Arquivo da foto do produto (máximo 500KB, apenas JPG e PNG)",
+				required = true)
+			MultipartFile arquivo) throws IOException;
+	
     void excluir(
             @ApiParam(value = "ID do restaurante", example = "1", required = true)
             Long restauranteId,
