fdad6f6369ccf2c50157aa1777cb7d7fa0b57808
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-07-13 16:12:22 -0300

    https://www.algaworks.com/aulas/2196/corrigindo-a-paginacao-na-documentacao/

--- src/main/java/br/com/siswbrasil/algafood/api/openapi/model/CozinhasModelOpenApi.java
+++ src/main/java/br/com/siswbrasil/algafood/api/openapi/model/CozinhasModelOpenApi.java
@@ -1,9 +1,30 @@
 package br.com.siswbrasil.algafood.api.openapi.model;
 
+import java.util.List;
+
+import org.springframework.hateoas.Links;
+
 import br.com.siswbrasil.algafood.api.model.CozinhaModel;
 import io.swagger.annotations.ApiModel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @ApiModel("CozinhasModel")
-public class CozinhasModelOpenApi extends PagedModelOpenApi<CozinhaModel> {
+@Setter
+@Getter
+public class CozinhasModelOpenApi {
+	
+	private CozinhasEmbeddedModelOpenApi _embedded;
+	private Links _links;
+	private PageModelOpenApi page;
+	
+	@ApiModel("CozinhasEmbeddedModel")
+	@Data
+	public class CozinhasEmbeddedModelOpenApi {
+		
+		private List<CozinhaModel> cozinhas;
+		
+	}
 	
 }
