c6678fb4c94072ae3f5eda99b95460299e8c0831
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-07-13 07:28:00 -0300

    https://www.algaworks.com/aulas/2166/montando-modelo-de-representacao-com-representationmodelassembler/

--- src/main/java/br/com/siswbrasil/algafood/api/controller/CidadeController.java
+++ src/main/java/br/com/siswbrasil/algafood/api/controller/CidadeController.java
@@ -56,24 +56,7 @@ public class CidadeController implements CidadeControllerOpenApi {
 	public CollectionModel<CidadeModel> listar() {
 		List<Cidade> todasCidades = cidadeRepository.findAll();
 		
-		List<CidadeModel> cidadesModel = cidadeModelAssembler.toCollectionModel(todasCidades);
-		
-		cidadesModel.forEach(cidadeModel -> {
-			cidadeModel.add(linkTo(methodOn(CidadeController.class)
-					.buscar(cidadeModel.getId())).withSelfRel());
-			
-			cidadeModel.add(linkTo(methodOn(CidadeController.class)
-					.listar()).withRel("cidades"));
-			
-			cidadeModel.getEstado().add(linkTo(methodOn(EstadoController.class)
-					.buscar(cidadeModel.getEstado().getId())).withSelfRel());
-		});
-		
-		CollectionModel<CidadeModel> cidadesCollectionModel = new CollectionModel<>(cidadesModel);
-		
-		cidadesCollectionModel.add(linkTo(CidadeController.class).withSelfRel());
-		
-		return cidadesCollectionModel;
+		return cidadeModelAssembler.toCollectionModel(todasCidades);
 	}
 
 	@Override
@@ -81,29 +64,7 @@ public class CidadeController implements CidadeControllerOpenApi {
 	public CidadeModel buscar(@PathVariable Long cidadeId) {
 		Cidade cidade = cidadeService.buscarOuFalhar(cidadeId);
 		
-		CidadeModel cidadeModel = cidadeModelAssembler.toModel(cidade);
-		
-		cidadeModel.add(linkTo(methodOn(CidadeController.class)
-				.buscar(cidadeModel.getId())).withSelfRel());
-		
-//		cidadeModel.add(linkTo(CidadeController.class)
-//				.slash(cidadeModel.getId()).withSelfRel());
-		
-		cidadeModel.add(linkTo(methodOn(CidadeController.class)
-				.listar()).withRel("cidades"));
-		
-//		cidadeModel.add(linkTo(CidadeController.class)
-//				.withRel("cidades"));
-		
-		cidadeModel.getEstado().add(linkTo(methodOn(EstadoController.class)
-				.buscar(cidadeModel.getEstado().getId())).withSelfRel());
-		
-//		cidadeModel.getEstado().add(linkTo(EstadoController.class)
-//				.slash(cidadeModel.getEstado().getId()).withSelfRel());
-		
-		return cidadeModel;		
-		
-
+		return cidadeModelAssembler.toModel(cidade);
 	}
 
 	@Override
