5f757a94f85dd4e34d388f8b9b0d68ddb7bf24b6
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-07-13 18:31:38 -0300

    https://www.algaworks.com/aulas/2218/desafio-adicionando-os-recursos-de-cozinhas-na-v2-da-api/

--- src/main/java/br/com/siswbrasil/algafood/api/v2/AlgaLinksV2.java
+++ src/main/java/br/com/siswbrasil/algafood/api/v2/AlgaLinksV2.java
@@ -7,6 +7,7 @@ import org.springframework.hateoas.Link;
 import org.springframework.stereotype.Component;
 
 import br.com.siswbrasil.algafood.api.v2.controller.CidadeControllerV2;
+import br.com.siswbrasil.algafood.api.v2.controller.CozinhaControllerV2;
 
 @Component
 public class AlgaLinksV2 {
@@ -18,5 +19,15 @@ public class AlgaLinksV2 {
 	public Link linkToCidades() {
 		return linkToCidades(IanaLinkRelations.SELF.value());
 	}
+	
+	public Link linkToCozinhas(String rel) {
+	    return linkTo(CozinhaControllerV2.class).withRel(rel);
+	}
+
+	public Link linkToCozinhas() {
+	    return linkToCozinhas(IanaLinkRelations.SELF.value());
+	}
+
+
 	
 }
