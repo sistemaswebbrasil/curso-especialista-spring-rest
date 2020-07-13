c8cca8732d2a820229a9d9d7cbd41d6982269f7f
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-07-13 07:50:24 -0300

    https://www.algaworks.com/aulas/2168/corrigindo-link-de-colecao-de-recurso-de-responsaveis-por-restaurante/

--- src/main/java/br/com/siswbrasil/algafood/api/controller/RestauranteUsuarioResponsavelController.java
+++ src/main/java/br/com/siswbrasil/algafood/api/controller/RestauranteUsuarioResponsavelController.java
@@ -1,5 +1,8 @@
 package br.com.siswbrasil.algafood.api.controller;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.HttpStatus;
@@ -29,12 +32,16 @@ public class RestauranteUsuarioResponsavelController implements RestauranteUsuar
     @Autowired
     private UsuarioModelAssembler usuarioModelAssembler;
     
-    @GetMapping 
-    public CollectionModel<UsuarioModel> listar(@PathVariable Long restauranteId) {
-        Restaurante restaurante = restauranteService.buscarOuFalhar(restauranteId);
-        
-        return usuarioModelAssembler.toCollectionModel(restaurante.getResponsaveis());
-    }
+	@Override
+	@GetMapping
+	public CollectionModel<UsuarioModel> listar(@PathVariable Long restauranteId) {
+		Restaurante restaurante = restauranteService.buscarOuFalhar(restauranteId);
+		
+		return usuarioModelAssembler.toCollectionModel(restaurante.getResponsaveis())
+				.removeLinks()
+				.add(linkTo(methodOn(RestauranteUsuarioResponsavelController.class)
+						.listar(restauranteId)).withSelfRel());
+	}
     
     @DeleteMapping("/{usuarioId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
