5d4cce10ac85af743e399495f50437a10d6d56d4
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-07-14 16:13:33 -0300

    https://www.algaworks.com/aulas/2243/configurando-o-client-credentials-grant-type-no-authorization-server/

--- algafood-auth/src/main/java/br/com/siswbrasil/algafood/auth/AuthorizationServerConfig.java
+++ algafood-auth/src/main/java/br/com/siswbrasil/algafood/auth/AuthorizationServerConfig.java
@@ -11,6 +11,17 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
@@ -34,6 +45,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 				.scopes("write", "read")
 				.accessTokenValiditySeconds(6 * 60 * 60)// 6 horas
 				.refreshTokenValiditySeconds(60 * 24 * 60 * 60) // 60 dias
+			
+			.and()
+				.withClient("faturamento")
+				.secret(passwordEncoder.encode("faturamento123"))
+				.authorizedGrantTypes("client_credentials")
+				.scopes("write", "read")
+				
 			.and()
 				.withClient("checktoken")
 					.secret(passwordEncoder.encode("check123"));
