40c150288ec7137cc5cd40df95ef60da7da2dd39
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-07-14 13:46:29 -0300

    https://www.algaworks.com/aulas/2235/criando-o-projeto-do-authorization-server-com-spring-security-oauth2/?pagina=0

---
+++ algafood-auth/src/main/java/br/com/siswbrasil/algafood/auth/AuthorizationServerConfig.java
@@ -0,0 +1,11 @@
+package br.com.siswbrasil.algafood.auth;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+}
