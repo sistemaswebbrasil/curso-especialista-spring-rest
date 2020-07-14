54215cfea40bb7d3d08ebe4647744b618ca57cd9
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-07-14 14:30:27 -0300

    https://www.algaworks.com/aulas/2236/configurando-o-fluxo-authorization-server-com-password-credentials-e-opaque-tokens/?pagina=0

--- algafood-auth/src/main/java/br/com/siswbrasil/algafood/auth/WebSecurityConfig.java
+++ algafood-auth/src/main/java/br/com/siswbrasil/algafood/auth/WebSecurityConfig.java
@@ -2,6 +2,7 @@ package br.com.siswbrasil.algafood.auth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -28,5 +29,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		return super.authenticationManager();
+	}	
 	
 }
