e072fba2b5ca29c1871767da9243cfcdbb01b9f1
Adriano Faria Alves <adriano.faria@outlook.com.br>
2020-07-14 15:48:33 -0300

    https://www.algaworks.com/aulas/2240/configurando-o-refresh-token-grant-type-no-authorization-server/

--- algafood-auth/src/main/java/br/com/siswbrasil/algafood/auth/AuthorizationServerConfig.java
+++ algafood-auth/src/main/java/br/com/siswbrasil/algafood/auth/AuthorizationServerConfig.java
@@ -3,6 +3,7 @@ package br.com.siswbrasil.algafood.auth;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -20,15 +21,18 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
+	@Autowired
+	private UserDetailsService userDetailsService;
+	
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients
 			.inMemory()
 				.withClient("algafood-web")
 				.secret(passwordEncoder.encode("web123"))
-				.authorizedGrantTypes("password")
+				.authorizedGrantTypes("password", "refresh_token")
 				.scopes("write", "read")
-				.accessTokenValiditySeconds(60 * 60 * 6) // 6 horas (padrão é 12 horas)
+				.accessTokenValiditySeconds(60 * 60 * 6)
 			.and()
 				.withClient("checktoken")
 					.secret(passwordEncoder.encode("check123"));
@@ -38,11 +42,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 //		security.checkTokenAccess("isAuthenticated()");
 		security.checkTokenAccess("permitAll()");
-	}	
+	}
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints.authenticationManager(authenticationManager);
+		endpoints
+			.authenticationManager(authenticationManager)
+			.userDetailsService(userDetailsService);
 	}
 	
 }
